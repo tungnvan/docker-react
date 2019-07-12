849099dd83722fc748367ef7a767979f54ac91fd
TungNguyen <tungnvan.mar12th@gmail.com>
2019-07-12 12:51:21 +0700

    Do not run

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/tungnvan-docker-react
 docker build -t tungnvan-docker-react .
-docker run -p 80:80 --name tungnvan-docker-react tungnvan-docker-react
+# docker run -p 80:80 --name tungnvan-docker-react tungnvan-docker-react
